3ebe5aba260f71d255110287715e01afd4e4af84
Gabriel Mancinas <125517545+Gxboyz4@users.noreply.github.com>
2024-11-05 19:37:44 -0700

    Inicio mapeo inventario
    Inicio del mapeo del inventario

--- controlinventario/src/main/java/com/itson/controlinventario/ControlinventarioApplication.java
+++ controlinventario/src/main/java/com/itson/controlinventario/ControlinventarioApplication.java
@@ -2,12 +2,19 @@ package com.itson.controlinventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.itson.controlinventario.controller.ProductoController;
+import com.itson.controlinventario.modelos.Producto;
 
 @SpringBootApplication
 public class ControlinventarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControlinventarioApplication.class, args);
+		ProductoController productoController = new ProductoController();
+		for (Producto producto : productoController.getProductos()) {
+			System.out.println(producto.getNombre());
+		}
+
 	}
 
 }
