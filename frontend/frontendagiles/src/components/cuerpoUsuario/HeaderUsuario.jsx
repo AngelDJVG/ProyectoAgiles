e8aaba82d74f58efcba390ed56a774af4ec45973
MarioZDR <125427714+MarioZDR@users.noreply.github.com>
2024-11-28 17:07:02 -0700

    Integración de RabbitMQ
    Publicación al exchange de promociones desde el controlador de promociones en el backend.
    
    Consumo de mensajes/promociones con stompjs desde el frontend.

--- frontend/frontendagiles/src/components/cuerpoUsuario/HeaderUsuario.jsx
+++ frontend/frontendagiles/src/components/cuerpoUsuario/HeaderUsuario.jsx
@@ -1,17 +1,26 @@
 import '../../estilos/cuerpoUsuario/HeaderUsuario.css';
-import React from 'react';
+import React, { useState } from 'react';
 import LogoutButton from '../LogoutButton';
+import StompClient from './StompClient';
+import Promocion from '../../models/Promocion';
 
 function HeaderUsuario({ onSearchChange, setCarrito }) {
+	
 	const handleInputChange = (e) => {
 		const value = e.target.value;
 		if (onSearchChange) {
-			onSearchChange(value); // Llamar al handler pasado desde el padre
+			onSearchChange(value); 
 		}
+	};
+
+	const handleNewMessage = (message) => {
+		let promocion = new Promocion(message.id, message.id_producto, message.monto, message.fechaInicio, message.fechaFin);
+		alert(`Nueva promocion: ${promocion.id_producto} con un descuento de ${promocion.monto}`);
 	};
 
 	return (
 		<>
+			<StompClient onMessage={handleNewMessage} />
 			<header className="header">
 				<nav className="navbar">
 					<img src="/assets/Logo.png" alt="carrito" className="titulo" onClick={()=>{setCarrito(false)}} />
---
+++ frontend/frontendagiles/src/models/Promocion.js
@@ -0,0 +1,9 @@
+export default class Promocion{
+    constructor(id, id_producto, monto, fechaInicio, fechaFin){
+        this.id = id;
+        this.id_producto = id_producto;
+        this.monto = monto;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+}
